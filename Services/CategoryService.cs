a481c33dded18795f411e4e3c7326a58ed686b53
assis-silva <assislimacontato@gmail.com>
2020-12-05 02:40:28 -0300

    First commit

---
+++ Services/CategoryService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Domain.Services;
+
+namespace Supermarket.API.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+
+        public Task<IEnumerable<Category>> List()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _categoryRepository.ListAsync();
+        }
+    }
+}
